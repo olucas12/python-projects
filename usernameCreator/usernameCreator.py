b0debef7c131e87ca2bd509d0bf8537479bae2f2
olucas12 <lucashenriqsilva@hotmail.com>
2024-06-24 21:19:05 -0300

    usernamecreator

---
+++ usernameCreator/usernameCreator.py
@@ -0,0 +1,33 @@
+﻿"""
+Código para criação de usuário padrão a partir do nome.
+"""
+
+import os
+import unidecode  # Necessita instalação (pip install unidecode)
+import random
+
+os.system('cls' if os.name=='nt' else 'clear')
+
+nome = input('Digite o nome: ')
+nome = unidecode.unidecode(nome)  # Retira os acentos
+
+corte = nome.lower().split()
+
+primeiroNome = corte[0]
+
+sobrenome = corte[len(corte)-1]
+posicaoSobrenome = len(corte)-1
+
+meio = ''
+limit = 0
+
+for nome in corte[1:posicaoSobrenome]:
+    if len(nome) > 3 and limit <3:
+        for letra in nome:
+            meio += letra
+            limit+=1
+            break
+
+numero = random.randint(0,999)
+
+print(f'\n{primeiroNome}.{meio}{sobrenome}{numero:02}\n')
