2d96f329f8333aceacb930569d6084c8a80f3f54
olucas12 <lucashenriqsilva@hotmail.com>
2024-06-25 20:11:33 -0300

    passwordGenerator

---
+++ passwordGenerator/passwordGenerator.py
@@ -0,0 +1,11 @@
+﻿import random
+import os
+
+os.system('cls' if os.name=='nt' else 'clear')
+
+chars = 'abcdefghijklmnopqrstuvwxyz1234567890!@#$%*()_=+§¹²³£¢¬{[]}\|~^`´.,<>;:?°'
+
+for c in range(16):
+    print(f'{chars[random.randint(0, len(chars)-1)]}', end='')
+
+print('\n')
